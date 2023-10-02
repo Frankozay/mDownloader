445bcce07c3f721fb1e2403d3ce8cc9b639e44bd
frankozay <2068307656@qq.com>
2023-10-03 04:59:32 +0800

    添加项目文件。

---
+++ mDownloader/Models/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace mDownloader.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<DownloadTask> DownloadTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DownloadTask>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Download__3214EC07AC322252");
+
+            entity.Property(e => e.Destination).HasMaxLength(1000);
+            entity.Property(e => e.Url).HasMaxLength(1000);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ mDownloader/Models/DownloadTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mDownloader.Models;
+
+public partial class DownloadTask
+{
+    public int Id { get; set; }
+
+    public string Url { get; set; } = null!;
+
+    public string Destination { get; set; } = null!;
+
+    public DateTime DateCreated { get; set; }
+
+    public DateTime? DateFinished { get; set; }
+
+    public long TotalBytesToDownload { get; set; }
+
+    public int? StatusCode { get; set; }
+
+    public int Status { get; set; }
+
+    public bool IsQueued { get; set; }
+}
